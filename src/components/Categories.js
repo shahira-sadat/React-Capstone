a18697e002f294a9e767c64f978dae45c40af831
shahira-sadat <shahira.sadat1@gmail.com>
2022-08-30 22:23:52 +0430

    Filter leagues and Update some files

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,23 +1,32 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchLeagues } from '../redux/leagues/leagues';
 import './Categories.module.css';
 
 function Categories() {
-  // const leagues = useSelector((state) => state.leagues);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchLeagues);
   }, []);
 
+  const leagues = useSelector((state) => state.leagues);
+  console.log(leagues);
+
+  const [league, setLeague] = useState();
+  console.log(league);
+
+  const displayLeague = (id) => {
+    setLeague(() => [...leagues.filter((l) => l.league_id === id)]);
+  };
+
   return (
     <div>
       <div>
-        <button type="button">Premier League</button>
-        <button type="button">Serie A</button>
-        <button type="button">La Liga</button>
-        <button type="button">Ligue 1</button>
+        <button type="button" onClick={() => displayLeague(39)}>Premier League</button>
+        <button type="button" onClick={() => displayLeague(135)}>Serie A</button>
+        <button type="button" onClick={() => displayLeague(140)}>La Liga</button>
+        <button type="button" onClick={() => displayLeague(186)}>Ligue 1</button>
       </div>
     </div>
   );
--- src/redux/leagues/leagues.js
+++ src/redux/leagues/leagues.js
@@ -3,6 +3,9 @@ import axios from 'axios';
 // Action
 const LOAD_LEAGUES = 'football_leagues/leagues/LOAD_LEAGUES';
 
+// Action Creator
+const loadLeagues = (leagues) => ({ type: LOAD_LEAGUES, payload: leagues });
+
 // Reducer
 export default function leaguesReducer(state = [], action = {}) {
   switch (action.type) {
@@ -14,33 +17,13 @@ export default function leaguesReducer(state = [], action = {}) {
 }
 
 // Side Effects
-const fetchLeagues = async (dispatch, getState) => {
-  const currentLeagues = getState().leagues;
-
-  if (currentLeagues.length === 0) {
-    const { data } = await axios.get('https://v3.football.api-sports.io/leagues', {
-      headers: {
-        'x-rapidapi-host': 'v3.football.api-sports.io',
-        'x-rapidapi-key': 'efe4fd8bb72f9afb53d002282a5fb550',
-      },
+const fetchLeagues = async () => {
+  let leagues = [];
+  await axios.get('https://football-apis.herokuapp.com/api/v1/leagues')
+    .then((response) => {
+      leagues = response.data.leagues;
     });
-
-    const leagues = data.response
-      .filter(
-        (leagueObj) => leagueObj.league.id === 186
-          || leagueObj.league.id === 39
-          || leagueObj.league.id === 140
-          || leagueObj.league.id === 135,
-      )
-      .map((leagueObj) => ({
-        league_id: leagueObj.league.id,
-        league_name: leagueObj.league.name,
-        league_logo: leagueObj.league.logo,
-        league_country: leagueObj.country.name,
-      }));
-
-    dispatch({ type: LOAD_LEAGUES, payload: leagues });
-  }
+  return leagues;
 };
 
-export { fetchLeagues };
+export { fetchLeagues, loadLeagues };
