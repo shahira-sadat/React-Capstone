a0c1830ee7ebe96a5425cbc14307ceb47b6b931c
shahira-sadat <shahira.sadat1@gmail.com>
2022-08-30 22:02:52 +0430

    Add dependencies and update some files

--- src/index.js
+++ src/index.js
@@ -8,11 +8,10 @@ import store from './redux/configureStore';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-     <Provider store={store}>
+    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
 );
-
